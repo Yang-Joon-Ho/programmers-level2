153e6391daad3cd02f8e16dff52885a6a2b770bd
butcher313 <40017192+butcher313@users.noreply.github.com>
2020-07-31 18:12:02 +0900

    Create 파일명정렬.py

---
+++ 파일명정렬.py
@@ -0,0 +1,9 @@
+import re
+def solution(files):
+    # 1.
+    temp = [re.split(r"([0-9]+)", s) for s in files]
+    print(temp)
+    # 2.
+    sort = sorted(temp, key = lambda x: (x[0].lower(), int(x[1])))
+    # 3.
+    return ["".join(s) for s in sort]
